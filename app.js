ff370d37c6d7b7ed4f93b1cb70b33065b06dcb56
ipekozbay <20060401@stu.omu.edu.tr>
2023-05-13 15:12:05 +0300

    CRUD Operations

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const port = 3000;
 const hostname = '127.0.0.1';
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test_db',{
-    useNewUrlParsel:true,
-    useUnifiedTopology:true
-})
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
 
 app.use(express.static('public'));
 
--- test.js
+++ test.js
@@ -2,14 +2,41 @@ const mongoose = require('mongoose')
 
 const Post = require('./models/Post')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test_db',{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
+mongoose.connect('mongodb://127.0.0.1:27017/test_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
+Post.findByIdAndDelete('645f7a41cae1c4dabd5b4bd0')
+  .then(post => {
+
+    console.log(post);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+Post.findByIdAndUpdate('645f7a41cae1c4dabd5b4bd0', { title: "benim ikinci update postum" })
+  .then(post => {
+
+    console.log(post);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+Post.find({})
+  .then(post => {
+    console.log(post);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
 Post.create({
-  title: ' post title',
-  content: 'post content'
+  title: ' second post title',
+  content: 'second post content'
 })
   .then(post => {
     console.log(post);
