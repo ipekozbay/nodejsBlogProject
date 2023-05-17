78255d2b079ab08ee36f49a9fe3deba9cdeabd68
ipekozbay <20060401@stu.omu.edu.tr>
2023-05-17 22:45:11 +0300

    17.05.2023

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post')
 
 router.get('/', (req, res) => {
     res.render('site/index');
@@ -10,7 +11,10 @@ router.get('/about', (req, res) => {
 })
 
 router.get('/blog', (req, res) => {
-    res.render('site/blog');
+
+    Post.find({}).then(posts => {
+        res.render('site/blog',{posts:posts});
+    })
 })
 
 router.get('/contact', (req, res) => {
